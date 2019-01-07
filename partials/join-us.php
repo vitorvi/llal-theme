950ee9305e7f27fffba70f8fe85083a8e221e618
DESKTOP-TAGFRS8\Grande Circular <vtrmrx@gmail.com>
2019-01-07 16:28:52 -0200

    componente join-us

--- partials/join-us.php
+++ partials/join-us.php
@@ -7,5 +7,13 @@
 ?>
 
 <section class="join-us cinza-bg padding-top-large padding-bottom-large">
-
+    <div class="custom-container">
+        <div class="row">
+            <div class="col-12 col-lg-8 offset-lg-2 text-center">
+                <h2 class="text-center">Faça parte da nossa equipe</h2>
+                <p class="text-large text-center margin-top-small">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <a class="link-large margin-top-medium d-inline-block">Acesse aqui</a>
+            </div>
+        </div>
+    </div>
 </section>
