8980a3ecc3a4938c2bba016712fb6293cf03c2f4
Vítor Marques de Faria <vtrmrx@gmail.com>
2019-01-16 18:52:34 -0200

    cria nav do golden circle

--- partials/nav-golden-circle.php
+++ partials/nav-golden-circle.php
@@ -7,5 +7,42 @@
 ?>
 
 <section class="nav golden-circle branco-bg padding-top-large padding-bottom-large">
-
+  <div class="custom-container">
+    <div class="row padding-bottom-medium">
+      <div class="col-12">
+        <h2 class="text-center h1">Nosso Círculo de Ouro</h2>
+      </div>
+    </div>
+    <div class="row padding-top-medium">
+      <div class="col-12 col-lg-5">
+      </div>
+      <div class="col-12 offset-lg-1 col-lg-6">
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+          <li class="nav-item">
+            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">O quê</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Como</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Por quê</a>
+          </li>
+        </ul>
+        <div class="tab-content" id="myTabContent">
+          <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labor, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labor lorem ipsum dolor.</p>
+            <p>Sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Nossas especialidades consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+          </div>
+          <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labor, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labor lorem ipsum dolor.</p>
+            <p>Sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Nossas especialidades consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+          </div>
+          <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labor, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labor lorem ipsum dolor.</p>
+            <p>Sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Nossas especialidades consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
